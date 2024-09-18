ce2c0dc1609db29b0008679723c316c7a3bdd472
ilyasskurnia <ilyaskurnia07@gmail.com>
2024-09-19 00:02:55 +0700

    change all file

--- javascript/script1.js
+++
@@ -1,7 +0,0 @@
-var par = document.getElementsByClassName("par");
-setInterval(function () {
-par[0].style.color = "white";
-setTimeout(function () {
-par[0].style.color = "#FFB830";
-}, 500)
-}, 1000)
